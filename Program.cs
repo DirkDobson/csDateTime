9052fcdd83cd1e66b46f49e01caf55f973b8c711
Dirk Dobson <dirkvancouver@live.com>
2018-11-29 15:15:53 -0700

    different types of DateTime

--- Program.cs
+++ Program.cs
@@ -6,7 +6,21 @@ namespace csDateTime
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var dateTime = new DateTime(2018, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+
+            // System.Console.WriteLine("Hour" + now.Hour);
+            // System.Console.WriteLine("Minute" + now.Minute);
+
+            var tomorrow = now.AddDays(1);
+            var yesterday = now.AddDays(-1);
+
+            System.Console.WriteLine(now.ToLongDateString());
+            System.Console.WriteLine(now.ToShortDateString());
+            System.Console.WriteLine(now.ToLongTimeString());
+            System.Console.WriteLine(now.ToShortTimeString());
+            System.Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
         }
     }
 }
